d0f56faad988d43e253a4e6adfa5de40a8841a87
Alauddeen Shaikh <116334444+alauddeen@users.noreply.github.com>
2023-12-12 10:40:10 +0400

    robotcom.py

--- robotcom.py
+++ robotcom.py
@@ -2,17 +2,17 @@ import json
 import requests
 
 def send_command_to_node_mcu(NODEMCU_BASE_URL, command):
-    # Create a dictionary with the command
+    # Create a dictionary 
     command_dict = {'command': command}
-    # Convert the dictionary to a JSON string
+    # dictionary to a JSON string
     command_json = json.dumps(command_dict)
-    # Specify the content type as JSON
+    # content type as JSON
     headers = {'Content-Type': 'application/json'}
 
     print(f"Sending JSON: {command_json}")
 
     try:
-        # Send a POST request with the JSON data
+        # Send a POST request 
         response = requests.post(f"{NODEMCU_BASE_URL}/command", data=command_json, headers=headers)
         response.raise_for_status()  # Raise an error if the request failed
 
@@ -25,14 +25,12 @@ def send_command_to_node_mcu(NODEMCU_BASE_URL, command):
         print(f"Failed to send command to NodeMCU: {e}")
 
 
+def main():
+#This is for testing purposes 
+    NODEMCU_BASE_URL = 'http://192.168.183.102'
+    test_command = [200, 100, 200]
+    for command in test_command:
+        send_command_to_node_mcu(NODEMCU_BASE_URL, command)
 
-# def main():
-#     # This is for testing purposes and will not be present in the final integrated system.
-#     # Replace the URL with the actual URL of your NodeMCU server.
-#     NODEMCU_BASE_URL = 'http://192.168.183.102'
-#     test_command = [200, 100, 200]  # Example command sequence
-#     for command in test_command:
-#         send_command_to_node_mcu(NODEMCU_BASE_URL, command)
-
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
